cd672a826cb3670fcaee180ace7c85cfc09e0263
Jenny <jennifernguyen1992@gmail.com>
2016-06-28 13:21:23 -0500

    shiny app skeleton

---
+++ server.R
@@ -0,0 +1,34 @@
+
+library(plyr)
+library(stringr)
+library(ggplot2)
+library(jn.general)
+library(data.table)
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  in_x <- "apple"
+  in_y <- "banana"
+  
+  # function to split a string into vector of characters
+
+  # functions to convert each character position to a coordinate
+
+  # function to convert a click into a coordinate
+  
+  # make a grid of all pairwise combos of letters
+
+  # structure to hold the variables
+
+  
+  # plot to highlight results based on click coordinates 
+
+  
+  # function to draw the plot
+
+  
+  # generate plot output
+
+
+})
