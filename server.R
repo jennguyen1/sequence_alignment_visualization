402ca0d7a871eb7d5f170a5f4b9c67823d4c1190
Jenny <Nhuy@Jennifers-MacBook-Air.local>
2017-08-27 01:08:14 -0400

    ui comments; server move functions to separate section

--- server.R
+++ server.R
@@ -12,11 +12,176 @@ library(cowplot)
 library(jn.general)
 library(data.table)
 library(shiny)
+
 # load dependencies
 source('score_computation.R')
 source('generate_matrix.R')
 source('traceback.R')
 
+#-------------------------------------------------
+# FUNCTIONS FOR INITIATING DATA ------------------
+
+# function to split a string into vector of characters
+split_word <- function(word) laply(1:nchar(word), function(i) str_sub(word, i, i))
+
+
+# functions to convert each character position to a coordinate
+word_to_coordinates <- function(split_word) 0:(length(split_word) - 1) + 0.5
+
+# function to convert a click into a coordinate
+click_to_coordinates <- function(click, word_length, is_y = FALSE, original_coordinates){
+
+  # error for y data
+  if(is_y & missing(original_coordinates)){
+    stop("Need to supply original coordinates if mapping the y click")
+  }
+
+  # fix: boundary limits because ggdraw doesn't start and end at 0/1
+  if(is_y){
+    low = 0.05
+    high = 0.90
+  } else{
+    low = 0.10
+    high = 0.95
+  }
+
+  # set boundary limits
+  boundaries <- seq(low, high, length.out = word_length + 1)
+
+  # convert click value to coordinate
+  for(i in 1:(length(boundaries) - 1)){
+
+    if( between(click, boundaries[i], boundaries[i + 1]) ){
+      click_coordinate <- i - 0.5
+    }
+
+  }
+
+  if(is_y){
+    click_coordinate <- mapvalues(click_coordinate, original_coordinates, rev(original_coordinates))
+  }
+
+  # return results
+  click_coordinate <- laply(click_coordinate, function(a) a + 0.5)
+  return( click_coordinate )
+}
+
+
+# make a grid of all pairwise combos of letters
+make_data <- function(x, y) expand.grid(x = split_word(x), y = split_word(y))
+
+
+#-------------------------------------------------
+# FUNCTIONS FOR PLOTTING DATA --------------------
+
+# function to draw the plot
+drawPlot <- function(plot_data, params){
+
+  # plot data
+  if( is.numeric(plot_data$value) ){
+    g <- ggplot(data = plot_data, aes(x_coordinates, y_coordinates, label = text, fill = value)) +
+      geom_tile(color = "black", size = 1.1)
+  } else{
+    g <- ggplot(data = plot_data, aes(x_coordinates, y_coordinates, label = text)) +
+      geom_tile(fill = "#F0FFF0", color = "black", size = 1.1)
+  }
+
+  # plot data
+  g <- g +
+    geom_text() +
+    scale_fill_gradient(low = "#F0FFF0", high = "#0AC92B")
+
+  # format the axes
+  g <- g +
+    scale_y_reverse(breaks = params$coord_y, labels = params$split_y) +
+    scale_x_continuous(breaks = params$coord_x, labels = params$split_x) +
+    theme_bw() +
+    theme(
+      legend.position = "none",
+      panel.grid = element_blank(),
+      panel.border = element_blank(),
+      axis.text.x = element_text(size = 20, face = "bold"),
+      axis.text.y = element_text(size = 20, face = "bold"),
+      axis.ticks = element_line(color = "white")
+    ) +
+    labs(x = "", y = "")
+
+  # move x-axis to the top - note: this changes the coordinate system to [0,1] for both x/y axes
+  g <- ggdraw(switch_axis_position(g, axis = "x"))
+
+  return(g)
+}
+
+# process click to obtain strings & highlight plots
+process_click <- function(data, params, click){
+
+  return_nothing <- list(data = data, strigns = NULL)
+
+  # fix for no click data
+  if(is.null(click)){
+    return( return_nothing )
+  }
+
+  # fix for clicking outside of the boundaries - resets graph
+  if( !between(click$x, 0.10, 0.95) | !between(click$y, 0.05, 0.90) ){
+    return( return_nothing )
+  }
+
+  # find the coordinates of x and y
+  click_x <- click_to_coordinates(click$x, params$len_x, is_y = FALSE)
+  click_y <- click_to_coordinates(click$y, params$len_y, is_y = TRUE, params$coord_y)
+
+  # fix for clicking on the initial position (1, 1)
+  if(click_x == 1 & click_y == 1){
+    return( return_nothing )
+  }
+
+
+  # find the current matrix for affine gap values
+  if(params$gap != 0){
+
+    current_matrix <- llply(1:3, function(i) params$matrices[[i]][click_y, click_x])
+    names(current_matrix) <- names(params$matrices)
+    current_matrix <- names(which.max(current_matrix))
+
+  } else{
+
+    current_matrix <- NULL
+
+  }
+
+  # run the traceback and highlight the values
+  highlight_values <- traceback(
+    matrices = params$matrices, str_c = params$split_x, str_r = params$split_y,
+    current_matrix = current_matrix, current_row = click_y, current_col = click_x,
+    match = params$match, mismatch = params$mismatch, space = params$space, gap = params$gap, use_local = params$use_local
+  )
+
+  # fix for clicking on a box that doesn't lead anywhere
+  if( length(highlight_values$coordinates) == 0 ){
+    return( return_nothing )
+  }
+
+  # generate command to color the pathway
+  cmd <- lapply(highlight_values$coordinates, function(v) paste0("(y_coordinates == ", v[1] - 0.5, " & ", "x_coordinates == ", v[2] - 0.5, ")")) %>%
+    unlist %>%
+    paste(collapse = " | ")
+
+  # find the data to color
+  split_data <- to_be(data, dplyr::filter_, cmd)
+
+  # color data and return results
+  split_data$to_be <- mutate(split_data$to_be, value = 1)
+  split_data$not_to_be <- mutate(split_data$not_to_be, value = 0)
+  combined_results <- rbindlist(list(split_data$to_be, split_data$not_to_be))
+
+  # return results
+  return( list(data = combined_results, strings = highlight_values$strings) )
+
+}
+
+
+#-------------------------------------------------
 
 shinyServer(function(input, output) {
 
@@ -27,190 +192,10 @@ shinyServer(function(input, output) {
            "affine" = numericInput("space", "Space Score:", -2, max = 0)
     )
   })
-  
+
   output$gap_option <- renderUI({
     if(input$gap_penalty == "affine") numericInput("gap", "Gap Score:", -4, max = 0)
   })
-  
-  
-  ###################
-  # Initialize Data #
-  ###################
-
-  # function to split a string into vector of characters
-  split_word <- function(word) laply(1:nchar(word), function(i) str_sub(word, i, i))
-
-
-  # functions to convert each character position to a coordinate
-  word_to_coordinates <- function(split_word) 0:(length(split_word) - 1) + 0.5
-
-  # function to convert a click into a coordinate
-  click_to_coordinates <- function(click, word_length, is_y = FALSE, original_coordinates){
-
-    # error for y data
-    if(is_y & missing(original_coordinates)){
-      stop("Need to supply original coordinates if mapping the y click")
-    }
-
-    # fix: boundary limits because ggdraw doesn't start and end at 0/1
-    if(is_y){
-      low = 0.05
-      high = 0.90
-    } else{
-      low = 0.10
-      high = 0.95
-    }
-
-    # set boundary limits
-    boundaries <- seq(low, high, length.out = word_length + 1)
-
-    # convert click value to coordinate
-    for(i in 1:(length(boundaries) - 1)){
-      
-      if( between(click, boundaries[i], boundaries[i + 1]) ){
-        click_coordinate <- i - 0.5
-      }
-      
-    }
-
-    if(is_y){
-      click_coordinate <- mapvalues(click_coordinate, original_coordinates, rev(original_coordinates))
-    }
-
-    # return results
-    click_coordinate <- laply(click_coordinate, function(a) a + 0.5)
-    return( click_coordinate )
-  }
-
-
-  # make a grid of all pairwise combos of letters
-  make_data <- function(x, y) expand.grid(x = split_word(x), y = split_word(y))
-
-
-  #######################
-  # Plot Output Editing #
-  #######################
-  # function to draw the plot
-  drawPlot <- function(plot_data, params){
-
-    # plot data
-    if( is.numeric(plot_data$value) ){
-      g <- ggplot(data = plot_data, aes(x_coordinates, y_coordinates, label = text, fill = value)) +
-        geom_tile(color = "black", size = 1.1)
-    } else{
-      g <- ggplot(data = plot_data, aes(x_coordinates, y_coordinates, label = text)) +
-        geom_tile(fill = "#F0FFF0", color = "black", size = 1.1)
-    }
-    
-    # plot data
-    g <- g +
-      geom_text() + 
-      scale_fill_gradient(low = "#F0FFF0", high = "#0AC92B")
-
-    # format the axes
-    g <- g + 
-      scale_y_reverse(breaks = params$coord_y, labels = params$split_y) +
-      scale_x_continuous(breaks = params$coord_x, labels = params$split_x) +
-      theme_bw() +
-      theme(
-        legend.position = "none",
-        panel.grid = element_blank(),
-        panel.border = element_blank(),
-        axis.text.x = element_text(size = 20, face = "bold"),
-        axis.text.y = element_text(size = 20, face = "bold"),
-        axis.ticks = element_line(color = "white")
-      ) +
-      labs(x = "", y = "")
-
-    # move x-axis to the top - note: this changes the coordinate system to [0,1] for both x/y axes
-    g <- ggdraw(switch_axis_position(g, axis = "x"))
-
-    return(g)
-  }
-
-  # process click to obtain strings & highlight plots
-  process_click <- function(data, params, click){
-    
-    return_nothing <- list(data = data, strigns = NULL)
-
-    # fix for no click data
-    if(is.null(click)){
-      return( return_nothing )
-    }
-    
-    # fix for clicking outside of the boundaries - resets graph
-    if( !between(click$x, 0.10, 0.95) | !between(click$y, 0.05, 0.90) ){
-      return( return_nothing )
-    }
-
-    # find the coordinates of x and y
-    click_x <- click_to_coordinates(click$x, params$len_x, is_y = FALSE)
-    click_y <- click_to_coordinates(click$y, params$len_y, is_y = TRUE, params$coord_y)
-
-    # fix for clicking on the initial position (1, 1)
-    if(click_x == 1 & click_y == 1){
-      return( return_nothing )
-    }
-    
-    
-    # find the current matrix for affine gap values
-    if(params$gap != 0){
-      
-      current_matrix <- llply(1:3, function(i) params$matrices[[i]][click_y, click_x])
-      names(current_matrix) <- names(params$matrices)
-      current_matrix <- names(which.max(current_matrix))
-      
-    } else{
-      
-      current_matrix <- NULL
-      
-    }
-
-    # run the traceback and highlight the values
-    highlight_values <- traceback(
-      matrices = params$matrices, str_c = params$split_x, str_r = params$split_y,
-      current_matrix = current_matrix, current_row = click_y, current_col = click_x,
-      match = params$match, mismatch = params$mismatch, space = params$space, gap = params$gap, use_local = params$use_local
-    )
-
-    # fix for clicking on a box that doesn't lead anywhere
-    if( length(highlight_values$coordinates) == 0 ){
-      return( return_nothing )
-    }
-
-    # generate command to color the pathway
-    cmd <- lapply(highlight_values$coordinates, function(v) paste0("(y_coordinates == ", v[1] - 0.5, " & ", "x_coordinates == ", v[2] - 0.5, ")")) %>%
-      unlist %>%
-      paste(collapse = " | ")
-
-    # find the data to color
-    split_data <- to_be(data, dplyr::filter_, cmd)
-
-    # color data and return results
-    split_data$to_be <- mutate(split_data$to_be, value = 1)
-    split_data$not_to_be <- mutate(split_data$not_to_be, value = 0)
-    combined_results <- rbindlist(list(split_data$to_be, split_data$not_to_be))
-
-    # return results
-    return( list(data = combined_results, strings = highlight_values$strings) )
-
-  }
-
-  # effects of click on plot (highlights) disappears seconds after event
-  # ensure that the effect of click on graph is permanent by using a global click variable
-  click_value <- NULL
-  change_click <- function(previous, in_click){
-
-    if( (is.null(in_click) & is.null(previous)) ){
-      click_value <<- NULL
-    } else if( is.null(in_click) & !is.null(previous) ){
-      click_value <<- previous
-    } else{
-      click_value <<- in_click
-    }
-
-  }
-
 
   ################
   # Data Storage #
@@ -221,7 +206,7 @@ shinyServer(function(input, output) {
 
     # process gap value
     gap <- ifelse( input$gap_penalty == "linear", 0, input$gap )
-      
+
     # reset click_value
     click_value <<- NULL
 
@@ -243,9 +228,9 @@ shinyServer(function(input, output) {
 
     # make matrix: x = row indices; y = col indices
     DP_matrix <- make_matrices(
-      str_c = split_x, str_r = split_y, 
-      match = input$match, mismatch = input$mismatch, 
-      space = input$space, gap = gap, 
+      str_c = split_x, str_r = split_y,
+      match = input$match, mismatch = input$mismatch,
+      space = input$space, gap = gap,
       use_local = input$alignment == "local"
     )
     matrices <- DP_matrix$matrices
@@ -263,6 +248,21 @@ shinyServer(function(input, output) {
          space = input$space, gap = gap, use_local = input$alignment == "local"
     )
   })
+
+  # effects of click on plot (highlights) disappears seconds after event
+  # ensure that the effect of click on graph is permanent by using a global click variable
+  click_value <- NULL
+  change_click <- function(previous, in_click){
+
+    if( (is.null(in_click) & is.null(previous)) ){
+      click_value <<- NULL
+    } else if( is.null(in_click) & !is.null(previous) ){
+      click_value <<- previous
+    } else{
+      click_value <<- in_click
+    }
+
+  }
 
   # structure to hold plot highlights and alignment results
   clicks <- reactive({
--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@
 library(shiny)
 library(shinydashboard)
 
-dashboardPage(skin = "green", 
+dashboardPage(skin = "green",
 
   # app title
   dashboardHeader(title = "Sequence Alignment"),
@@ -16,53 +16,55 @@ dashboardPage(skin = "green",
 
   # UI body
   dashboardBody( verticalLayout(
-    
+
     # inputs
     box(
-      title = "Choose Input Parameters", width = NULL, 
+      title = "Choose Input Parameters", width = NULL,
       solidHeader = TRUE, status = "success", collapsible = TRUE,
-    
+
       tabBox(
         width = NULL, selected = "Input Data",
+        # instructions panel
         tabPanel(
           "Instructions",
-          p("This is an interactive shiny visualization of the Smith-Waterman/Needleman-Wunsch sequence alignment algorithm. These algorithms are used to align two sequences. It is commonly used to match DNA sequences."), 
-          
+          p("This is an interactive shiny visualization of the Smith-Waterman/Needleman-Wunsch sequence alignment algorithm. These algorithms are used to align two sequences. It is commonly used to match DNA sequences."),
+
           p("The ", span(strong("match score")),  " is added if two elements match. The ", span(strong("mismatch score")), " is the penalty if two elements do not match."),
           p("The ", span(strong("alignment")), " may be local (Smith Waterman algorithm) or global (Needleman-Wunsch algorithm). Global alignment finds the best match between entire sequences. Local alignment finds the best match between substrings of the two sequences."),
-          p("The ", span(strong("gap penalty")), " is the penalty for introducing gaps into a sequence. Affine gap penalties have a gap score, the penalty for opening a gap, and a space score, the penalty for extending a gap. Linear gap penalties treat each gap the same."), 
+          p("The ", span(strong("gap penalty")), " is the penalty for introducing gaps into a sequence. Affine gap penalties have a gap score, the penalty for opening a gap, and a space score, the penalty for extending a gap. Linear gap penalties treat each gap the same."),
           p("Activate the algorithm by clicking on the Submit button. The dynamic programming matrix will appear in the plot below. Click on any cell in the plot to find the corresponding best alignment. Click anywhere in the margins of the plot to reset the plot.")
         ),
-        
+
+        # user inputs panel
         tabPanel(
           "Input Data",
           fluidRow(
             column(width = 6, textInput("x", "Sequence 1:", value = "ttagagt")),
             column(width = 6, textInput("y", "Sequence 2:", value = "atagggtta"))
-          ), 
+          ),
           fluidRow(
             column(width = 6, radioButtons("alignment", "What kind of alignment?", c("local", "global"))),
             column(width = 6, radioButtons("gap_penalty", "What kind of gap penalty?", c("linear", "affine")))
-          ), 
+          ),
           fluidRow(
             column(width = 6, numericInput("match", "Match Score:", 1, min = 0)),
             column(width = 6, numericInput("mismatch", "Mismatch Score:", -1, max = 0))
           ),
           fluidRow(
-            column(width = 6, uiOutput("space_option")), 
+            column(width = 6, uiOutput("space_option")),
             column(width = 6, uiOutput("gap_option"))
           ),
           actionButton("submit", "Submit")
         )
       )
-      
+
     ),
-  
+
     # outputs
     box(
       title = "Alignment Results", width = NULL,
       solidHeader = TRUE, status = "success",
-      
+
       plotOutput("myPlot", click = "plotClick", height = "700px"),
 
       h3(textOutput("alignResults"), style = "font-family:courier"),
@@ -70,6 +72,6 @@ dashboardPage(skin = "green",
       h4(textOutput("alignedBars"), align = "center", style = "font-family:courier"),
       h4(textOutput("alignedText2"), align = "center", style = "font-family:courier")
     )
-    
+
   ))
 )
